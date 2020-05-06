bbb90c1f60b3bfbd7ec620af51ae2cf5396c95ea
Samir Ehsani <56870076+samirehsani19@users.noreply.github.com>
2020-05-06 15:30:58 +0200

    Samirehsani19
    Created model for join table between passenger and TimeTable

---
+++ AirLineAPI/Model/PassengerTimeTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLineAPI.Model
+{
+    public class PassengerTimeTable
+    {
+        public int PassengerId { get; set; }
+        public Passenger Passenger { get; set; }
+        public int TimeTableId { get; set; }
+        public TimeTable TimeTable { get; set; }
+    }
+}
