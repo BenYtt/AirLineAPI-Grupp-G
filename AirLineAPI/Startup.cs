7433e356489125984123449874daf1f38118fed0
Samir Ehsani <56870076+samirehsani19@users.noreply.github.com>
2020-05-06 16:28:00 +0200

    Created first migration

--- AirLineAPI/Startup.cs
+++ AirLineAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AirLineAPI.Db_Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -17,6 +18,7 @@ namespace AirLineAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddDbContext<AirLineContext>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
