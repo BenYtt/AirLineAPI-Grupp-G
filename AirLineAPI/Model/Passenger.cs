22bc5fe499c146750e0151a5c6660f07a9930932
Eric Sandberg <56867936+kaptenen@users.noreply.github.com>
2020-05-06 14:29:46 +0200

    Fixed spelling on Passenger

---
+++ AirLineAPI/Model/Passenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLineAPI.Model
+{
+    public class Passenger
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public int PersonalNumber { get; set; }
+        public TimeTable TimeTable { get; set; }
+    }
+}
