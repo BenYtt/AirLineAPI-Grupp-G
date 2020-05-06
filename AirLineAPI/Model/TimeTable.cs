51fa1fc2d86068ddcea7399bb9e4a6f600151e27
Eric Sandberg <56867936+kaptenen@users.noreply.github.com>
2020-05-06 14:33:11 +0200

    Added timetable model

--- AirLineAPI/Model/TimeTable.cs
+++ AirLineAPI/Model/TimeTable.cs
@@ -7,5 +7,10 @@ namespace AirLineAPI.Model
 {
     public class TimeTable
     {
+        public long ID { get; set; }
+        public Route Route { get; set; }
+        public Flight Flight { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
     }
 }
