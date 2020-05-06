68b122a37c00b66882f740156289661d26b2a0ae
Benjamin Ytterström <benjamin.ytterstrom@gmail.com>
2020-05-06 14:08:04 +0200

    Added FlightsController

---
+++ AirLineAPI/Controllers/FlightsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirLineAPI.Controllers
+{
+    [Route("api/v1.0/[controller]")]
+    public class FlightsController : ControllerBase
+    {
+        private readonly AirLineContext _context;
+
+        public FlightsController(AirLineContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var fakecontext = _context.Fakes.FirstOrDefault().Name;
+            return fakecontext;
+        }
+    }
+}
