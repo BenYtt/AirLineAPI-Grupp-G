9b6b9587c37c8b6ccbc753b5cd950344436ffa48
Eric Sandberg <56867936+kaptenen@users.noreply.github.com>
2020-05-06 14:21:04 +0200

    Added Passanger Model

--- AirLineAPI/Model/Passanger.cs
+++ AirLineAPI/Model/Passanger.cs
@@ -7,5 +7,9 @@ namespace AirLineAPI.Model
 {
     public class Passanger
     {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public int PersonalNumber { get; set; }
+        public TimeTable TimeTable { get; set; }
     }
 }
