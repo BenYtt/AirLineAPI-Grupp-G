b27c965a5c064b6b0e2e63988b657a044cf1ed9e
Benjamin Ytterström <benjamin.ytterstrom@gmail.com>
2020-05-06 15:37:00 +0200

    Added RoutesController

---
+++ AirLineAPI/Controllers/RoutesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AirLineAPI.Db_Context;
+
+namespace AirLineAPI.Controllers
+{
+    [Route("api/v1.0/[controller]")]
+    public class RoutesController : ControllerBase
+    {
+        private readonly AirLineContext _context;
+
+        public RoutesController(AirLineContext context)
+        {
+            _context = context;
+        }
+    }
+}
