1f9bb9a9af2daab16a15496b3d9f97a9934e5b84
Benjamin Ytterström <benjamin.ytterstrom@gmail.com>
2020-05-06 15:42:21 +0200

    Removed whitespace in destinationscontroller

--- AirLineAPI/Controllers/DestinationsController.cs
+++ AirLineAPI/Controllers/DestinationsController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AirLineAPI.Db_Context;
 
 namespace AirLineAPI.Controllers
 {
-    public class DestinationsController : Controller
+    [Route("api/v1.0/[controller]")]
+    public class DestinationsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly AirLineContext _context;
+        public DestinationsController(AirLineContext context)
         {
-            return View();
+            _context = context;
         }
     }
 }
