f5573ab83f21bcc30991184d8afcacd1a0766126
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-05-07 17:06:29 +0200

    added PassengerDTO

---
+++ AirLineAPI/Dto/PassengerView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLineAPI.Dto
+{
+    public class PassengerView
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public int IdentificationNumber { get; set; }
+        public List<TimeTableView> TimeTables { get; set; }
+    }
+}
