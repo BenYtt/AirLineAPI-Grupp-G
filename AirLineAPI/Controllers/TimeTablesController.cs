3630df2ae87b7be7fe4ffb5558bc780c2ad70645
Samir Ehsani <56870076+samirehsani19@users.noreply.github.com>
2020-05-07 10:28:41 +0200

    Created Repository and interface for controllers
    Created Repository and interface for controllers

--- AirLineAPI/Controllers/TimeTablesController.cs
+++ AirLineAPI/Controllers/TimeTablesController.cs
@@ -4,17 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AirLineAPI.Db_Context;
+using AirLineAPI.Repository;
 
 namespace AirLineAPI.Controllers
 {
     [Route("api/v1.0/[controller]")]
     public class TimeTablesController : ControllerBase
     {
+        private ITimeTableRepository repo;
         private readonly AirLineContext _context;
 
         public TimeTablesController(AirLineContext context)
         {
             _context = context;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTimeTableById(int id)
+        {
+            try
+            {
+                var timeTbale = await repo.GetTimeTableById(id);
+                if (timeTbale == null)
+                {
+                    return NotFound();
+                }
+                return Ok(timeTbale);
+
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+
     }
 }
