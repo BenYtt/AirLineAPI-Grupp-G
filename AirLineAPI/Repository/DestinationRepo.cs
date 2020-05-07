d13c04b828f16590fe20cea7eaf80aac72c2ad1e
Samir Ehsani <56870076+samirehsani19@users.noreply.github.com>
2020-05-07 12:29:14 +0200

    Created Dto and get request for Route, Destination, TimeTable
    Created Dto and get request for Route, Destination, TimeTable

---
+++ AirLineAPI/Repository/DestinationRepo.cs
@@ -0,0 +1,45 @@
+﻿using AirLineAPI.Db_Context;
+using AirLineAPI.Dto;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLineAPI.Repository
+{
+    public class DestinationRepo
+    {
+        public class TimeTableRepo : IDestinationRepository
+        {
+            private readonly AirLineContext db;
+            public TimeTableRepo(AirLineContext context)
+            {
+                this.db = context;
+            }
+
+            public async Task<DestinationView> GetDestinationById(int id)
+            {
+                if (db != null)
+                {
+                    return await (from d in db.Destinations
+                                  from f in db.Flights
+                                  from t in db.TimeTables
+                                  where d.ID == id
+                                  select new DestinationView
+                                  {
+                                      ID = d.ID,
+                                      FlightName = f.Model,
+                                      Country = d.Country,
+                                      City = d.City,
+                                      DepartureTime = t.DepartureTime,
+                                      ArrivalTime = t.ArrivalTime
+                                  }).FirstOrDefaultAsync();
+                }
+
+                return null;
+            }
+
+        }
+    }
+}
---
+++ AirLineAPI/Repository/RouteRepo.cs
@@ -0,0 +1,37 @@
+﻿using AirLineAPI.Db_Context;
+using AirLineAPI.Dto;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLineAPI.Repository
+{
+    class RouteRepo : IRouteRepository
+    {
+        private AirLineContext db;
+        public RouteRepo(AirLineContext contex)
+        {
+            this.db = contex;
+        }
+        public async Task<RouteView> GetRouteById(int id)
+        {
+            if (db!=null)
+            {
+                return await (from r in db.Routes
+                             where r.ID == id
+                             select new RouteView
+                             {
+                                 ID = r.ID,
+                                 Name = r.Name,
+                                 //StartDestination=r.StartDestination,
+                                 //EndDestination=r.EndDestination,
+                                 TravelTime = r.TravelTime
+
+                             }).FirstOrDefaultAsync();
+            }
+            return null;
+        }
+    }
+}
