35695c8129ed73176c3175eb32f6d2d637f828d3
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-05-08 13:59:05 +0200

    added repo file for passenger

---
+++ AirLineAPI/Repository/PassengerRepo.cs
@@ -0,0 +1,15 @@
+﻿using AirLineAPI.Db_Context;
+using AirLineAPI.Dto;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLineAPI.Repository
+{
+    public class PassengerRepo
+    {
+        
+    }
+}
