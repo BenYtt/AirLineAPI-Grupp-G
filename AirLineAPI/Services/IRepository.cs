337245d862b4451ca8e52b1952b16ad14b94dfbe
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-05-11 14:11:16 +0200

    renamned Repository folder to Services and changed all namespaces

--- AirLineAPI/Services/IRepository.cs
+++ AirLineAPI/Services/IRepository.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AirLineAPI.Repository
+namespace AirLineAPI.Services
 {
     public interface ITimeTableRepository
     {
