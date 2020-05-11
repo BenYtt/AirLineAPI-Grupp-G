a141f79990dacf431f58487c972f1a777d67fcdd
Benjamin Ytterström <benjamin.ytterstrom@gmail.com>
2020-05-11 16:10:05 +0200

    Added interface TimeTableRepository

---
+++ AirLineAPI/Services/ITimeTableRepository.cs
@@ -0,0 +1,14 @@
+﻿using AirLineAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLineAPI.Services
+{
+    interface ITimeTableRepository : IRepository
+    {
+        Task<TimeTable[]> GetTimeTables(bool includePassangers = false, bool includeRoute = false);
+        Task<TimeTable> GetTimeTable(long FlightID, bool includePassangers = false, bool includeRoute = false);
+    }
+}
