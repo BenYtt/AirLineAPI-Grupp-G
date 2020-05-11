002b9783e3a43e725fdac4f401f883fd30f7d52d
Eric Sandberg <56867936+kaptenen@users.noreply.github.com>
2020-05-11 16:19:51 +0200

    Added IRoute that inharete from Irepository

---
+++ AirLineAPI/Services/IRouteRepository.cs
@@ -0,0 +1,14 @@
+﻿using AirLineAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLineAPI.Services
+{
+    interface IRouteRepository : IRepository
+    {
+        Task<Route[]> GetFlights();
+        Task<Route> GetFlight(long FlightID);
+    }
+}
