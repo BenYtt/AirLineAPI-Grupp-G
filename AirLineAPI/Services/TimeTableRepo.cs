1d0a51c315f819073f989dbeba33f549f8eb972e
Benjamin Ytterström <benjamin.ytterstrom@gmail.com>
2020-05-11 15:08:39 +0200

    Removed TimtableRepo Body

--- AirLineAPI/Services/TimeTableRepo.cs
+++ AirLineAPI/Services/TimeTableRepo.cs
@@ -12,31 +12,6 @@ namespace AirLineAPI.Services
 {
     public class TimeTableRepo: ITimeTableRepository
     {
-        private readonly AirLineContext db;
-        public TimeTableRepo(AirLineContext context)
-        {
-            this.db = context;
-        }
-
-        public async Task<TimeTableView> GetTimeTableById(int id)
-        {
-            if (db != null)
-            {
-                return await (from p in db.TimeTables
-                              from r in db.Routes
-                              from f in db.Flights
-                              where p.ID == id
-                              select new TimeTableView
-                              {
-                                 ID= p.ID,
-                                 FlightName= f.Manufacturer,
-                                 RouteName= r.Name,
-                                 DepartureTime=p.DepartureTime,
-                                 ArrivalTime=p.ArrivalTime
-                              }).FirstOrDefaultAsync();
-            }
-
-            return null;
-        }
+       
     }
 }
