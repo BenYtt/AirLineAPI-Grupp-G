410400b74f4fa31350145f2a04efadfb8f480e40
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-05-12 13:59:19 +0200

    added constrains to Destination table

--- AirLineAPI/Model/Destination.cs
+++ AirLineAPI/Model/Destination.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace AirLineAPI.Model
     public class Destination
     {
         public long ID { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "The CityColumn needs to be longer then 3 characters")]
         public string City { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "The CountryColumn needs to be longer then 3 characters")]
         public string Country { get; set; }
     }
 }
