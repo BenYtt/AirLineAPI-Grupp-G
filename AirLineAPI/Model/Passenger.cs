c84f469b4d66e2290a74daf0f04f2ef4075350d3
Samir Ehsani <56870076+samirehsani19@users.noreply.github.com>
2020-05-12 10:05:26 +0200

    added contraints for passenger table
    added contraints for passenger table

--- AirLineAPI/Model/Passenger.cs
+++ AirLineAPI/Model/Passenger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,12 @@ namespace AirLineAPI.Model
 {
     public class Passenger
     {
+        [Key]
         public long ID { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength =2, ErrorMessage ="Name must be longer than 2 characters and shorter than 30")]
         public string Name { get; set; }
-        public int PersonalNumber { get; set; }
+        public int IdentificationNumber { get; set; }
         public ICollection<PassengerTimeTable> PassengerTimeTables { get; set; }
     }
 
