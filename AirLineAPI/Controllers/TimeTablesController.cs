eabf58910d7cfbc3f6909762013be1ab8ea5fc12
Benjamin Ytterström <benjamin.ytterstrom@gmail.com>
2020-05-12 12:28:41 +0200

    Fixed TimeTablesController working with existing methods

--- AirLineAPI/Controllers/TimeTablesController.cs
+++ AirLineAPI/Controllers/TimeTablesController.cs
@@ -5,12 +5,47 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AirLineAPI.Db_Context;
 using AirLineAPI.Services;
+using AirLineAPI.Model;
+using Microsoft.AspNetCore.Http;
 
 namespace AirLineAPI.Controllers
 {
     [Route("api/v1.0/[controller]")]
     public class TimeTablesController : ControllerBase
     {
+        private readonly ITimeTableRepository _repository;
 
+        public TimeTablesController(ITimeTableRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TimeTable[]>> GetTimeTables([FromQuery]bool includePassengers = false, bool includeRoutes = false)
+        {
+            try
+            {
+                var results = await _repository.GetTimeTables(includePassengers, includeRoutes);
+                return Ok(results);
+            }
+            catch (Exception e)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Database Failure: {e.Message}");
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TimeTable[]>> GetTimeByID([FromQuery]long id, bool includePassengers = false, bool includeRoutes = false)
+        {
+            try
+            {
+                var results = await _repository.GetTimeTableByID(id, includePassengers, includeRoutes);
+                return Ok(results);
+            }
+            catch (Exception e)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Database Failure: {e.Message}");
+            }
+        }
     }
 }
