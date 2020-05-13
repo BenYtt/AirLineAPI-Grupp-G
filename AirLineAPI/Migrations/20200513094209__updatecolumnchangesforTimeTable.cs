7012c5815298332aff41f1d44a1e2c2e89bef152
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-05-13 11:45:34 +0200

    add new migration fro the columnchanges in TimeTable-table

---
+++ AirLineAPI/Migrations/20200513094209__updatecolumnchangesforTimeTable.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirLineAPI.Migrations
+{
+    public partial class _updatecolumnchangesforTimeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TimeTables_Flights_FlightID",
+                table: "TimeTables");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TimeTables_Routes_RouteID",
+                table: "TimeTables");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Passengers_IdentificationNumber",
+                table: "Passengers");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RouteID",
+                table: "TimeTables",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "FlightID",
+                table: "TimeTables",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DepartureTime",
+                table: "TimeTables",
+                type: "SMALLDATETIME",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ArrivalTime",
+                table: "TimeTables",
+                type: "SMALLDATETIME",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Flights",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Manufacturer",
+                table: "Flights",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Destinations",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Destinations",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TimeTables_Flights_FlightID",
+                table: "TimeTables",
+                column: "FlightID",
+                principalTable: "Flights",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TimeTables_Routes_RouteID",
+                table: "TimeTables",
+                column: "RouteID",
+                principalTable: "Routes",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TimeTables_Flights_FlightID",
+                table: "TimeTables");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TimeTables_Routes_RouteID",
+                table: "TimeTables");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RouteID",
+                table: "TimeTables",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "FlightID",
+                table: "TimeTables",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DepartureTime",
+                table: "TimeTables",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "SMALLDATETIME");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ArrivalTime",
+                table: "TimeTables",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "SMALLDATETIME");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Flights",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Manufacturer",
+                table: "Flights",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Passengers_IdentificationNumber",
+                table: "Passengers",
+                column: "IdentificationNumber",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TimeTables_Flights_FlightID",
+                table: "TimeTables",
+                column: "FlightID",
+                principalTable: "Flights",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TimeTables_Routes_RouteID",
+                table: "TimeTables",
+                column: "RouteID",
+                principalTable: "Routes",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
