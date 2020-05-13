727da802026d50e658617c790140e7968de5b0c0
Samir Ehsani <56870076+samirehsani19@users.noreply.github.com>
2020-05-13 09:55:47 +0200

    Changed attribute

--- AirLineAPI/Model/Passenger.cs
+++ AirLineAPI/Model/Passenger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,7 +15,7 @@ namespace AirLineAPI.Model
         [StringLength(30, MinimumLength =2, ErrorMessage ="Name must be longer than 2 characters and shorter than 30")]
         public string Name { get; set; }
         [Required(ErrorMessage ="Identification number is required")]
-        [Range(0, 9)]
+        [Index(IsUnique =true)]
         public long IdentificationNumber { get; set; }
         public ICollection<PassengerTimeTable> PassengerTimeTables { get; set; }
     }
