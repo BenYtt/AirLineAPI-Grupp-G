e49d1a0c509eea06de21c6a227e1821cca118bc0
irvin <irvin_b23@hotmail.com>
2020-05-18 13:14:53 +0200

    @irvper77 create configuration map

---
+++ AirLineAPI/Configuration/MappedProfile.cs
@@ -0,0 +1,13 @@
+﻿using AirLineAPI.Model;
+using AutoMapper;
+using AirLineAPI.Dto;
+
+
+namespace AirLineAPI.Configuration
+{
+    public class MappedProfile
+    {
+        CreateMap<Passenger, PassengerDto>()
+                .ReverseMap();
+    }
+}
