98a1fd810d0f7107be11c18096f0458febaf3e91
fjalling <jakob.fjalling@gmail.com>
2020-05-18 14:06:02 +0200

    @fjalling Almost done with Dto need to fix another issue first

--- AirLineAPI/Dto/DestinationDto.cs
+++ AirLineAPI/Dto/DestinationDto.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AirLineAPI.Dto
 {
-    public class DetinationDto
+    public class DestinationDto
     {
+        [Required]
+        public long ID { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
     }
 }
