c030567b32154235bf09535715dd25eff88e4be0
Benjamin Ytterström <benjamin.ytterstrom@gmail.com>
2020-05-20 13:08:57 +0200

    Added status call NotFound

--- AirLineAPI/Controllers/FlightsController.cs
+++ AirLineAPI/Controllers/FlightsController.cs
@@ -63,8 +63,13 @@ namespace AirLineAPI.Controllers
             try
             {
                 var results = await _repository.GetFlightsByManufacturer(manufacturer);
-                var mappedResult = _mapper.Map<FlightDto[]>(results);
+                
+                if (results == null)
+                {
+                    return NotFound($"Couldn't find destination {manufacturer}.");
+                }
 
+                var mappedResult = _mapper.Map<FlightDto[]>(results);
                 return Ok(mappedResult);
             }
             catch (Exception e)
