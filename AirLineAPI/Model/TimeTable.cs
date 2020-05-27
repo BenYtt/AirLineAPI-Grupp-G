4fb77d52a9bd5d25316ba759eae0f12c1763ee1b
fjalling <jakob.fjalling@gmail.com>
2020-05-27 10:15:53 +0200

    jalling statted work on swagger

--- AirLineAPI/Model/TimeTable.cs
+++ AirLineAPI/Model/TimeTable.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace AirLineAPI.Model
